13e855d37e6f297df65037a902f62dfd64be3b09
harksu <harksulim@naver.com>
2022-09-23 05:17:52 +0900

    Feat : adjust Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Header from './Components/Header';
 import GlobalStyle from './styles/GlobalStyles';
 import Theme from './styles/theme';
 import Footer from './Components/Footer';
 import LoginPage from './Pages/LoginPage';
-import DetailPage from './pages/DetailPage';
+import DetailPage from './Pages/DetailPage';
 
 function App() {
   return (
@@ -13,8 +14,10 @@ function App() {
       <GlobalStyle />
       <Header />
       <Footer />
-      <LoginPage />
-      <DetailPage />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
     </ThemeProvider>
   );
 }
