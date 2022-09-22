1aa4a5d8932c89d77c28c9ab71a4f9267dd07e0e
ChoiWonYu <phillip07290@naver.com>
2022-09-23 04:29:13 +0900

    Feat: DetailPage Structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import GlobalStyle from './styles/GlobalStyles';
 import Theme from './styles/theme';
 import Footer from './components/Footer';
-
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <GlobalStyle />
       <Header />
       <Footer />
+      <DetailPage />
     </ThemeProvider>
   );
 }
