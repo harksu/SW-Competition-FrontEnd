6e4553c87d1ac5e8cc420d96bf531a291a000622
JJongsKim <totnwjd530@gmail.com>
2022-09-23 22:37:44 +0900

    Style: 메인페이지 글 목록 헤더 스타일링

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -6,9 +6,23 @@ function MainPage() {
     <MainBg>
       <MainTitle>명지 의견 나눔함</MainTitle>
       <MainContainer>
-        <ListContainer />
+        <ListContainer>
+          <ListHeader>
+            <Header1stContent>
+              <p>최신순&nbsp;</p>|<p>&nbsp;인기순</p>
+            </Header1stContent>
+            <Header2ndContent>
+              <p>순번</p>
+              <p>제목</p>
+              <p>작성자</p>
+              <p>공감</p>
+              <p>답변</p>
+            </Header2ndContent>
+          </ListHeader>
+          <ListContents />
+        </ListContainer>
         <WriteButton>작성하기</WriteButton>
-        <ListPages />
+        <ListPagesButton />
       </MainContainer>
     </MainBg>
   );
@@ -20,9 +34,9 @@ const MainBg = styled.div`
 `;
 
 const MainTitle = styled.p`
-  font-size: 40px;
+  font-size: 36px;
   font-weight: 500;
-  margin: 55px 0 10px 53px;
+  margin: 50px 0 15px 53px;
 `;
 
 const MainContainer = styled.div`
@@ -31,28 +45,69 @@ const MainContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  height: 72%;
-  background-color: yellow;
+  height: 75%;
 `;
 
 const ListContainer = styled.div`
   width: 100%;
-  height: 80%;
-  border: 0.5px solid #000;
+  height: 100%;
+  border-radius: 10px;
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
 `;
 
-const ListPages = styled.div`
+const ListHeader = styled.div`
+  display: grid;
+  grid-template-rows: 55% 45%;
+  width: 100%;
+  height: 20%;
+`;
+
+const Header1stContent = styled.div`
+  display: flex;
+  margin: 32px 0 0 36px;
+
+  p {
+    cursor: pointer;
+  }
+`;
+
+// const SelectLineUp = styled.p``;
+
+const Header2ndContent = styled.div`
+  display: grid;
+  grid-template-columns: 7% 63% 10% 10% 10%;
+  border: 1px solid #b5b5b5;
+  background-color: #f1f1f1;
+  text-align: center;
+
+  p {
+    padding: 12px;
+    font-weight: 500;
+    font-size: 20px;
+  }
+`;
+
+const ListContents = styled.div`
+  /* width: 100%; */
+  /* height: 80%; */
+  background-color: pink;
+`;
+
+const ListPagesButton = styled.div`
   width: 350px;
   height: 40px;
-  border: 0.5px solid #000;
+  border: 0.5px solid #adadad;
   border-radius: 5px;
 `;
 
 const WriteButton = styled.button`
-  width: 152px;
-  height: 48px;
-  margin-left: 80%;
+  width: 138px;
+  height: 43px;
+  margin: 1.5% 0 0 80%;
+  border: 0.3px solid #b5b5b5;
   border-radius: 5px;
+  background-color: transparent;
+  font-size: 16px;
   cursor: pointer;
 `;
 
