48ccbd0e4abde38911b6f2223996847e00bd95d3
yuraup <o721@naver.com>
2022-10-02 00:54:48 +0900

    refactor: reactQueryDevTool 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-boolean-value */
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
@@ -17,7 +16,7 @@ import WritingPage from './Pages/WritingPage';
 import LoadingPage from './Pages/LoadingPage';
 import MainPage from './Pages/MainPage';
 import SignUpPage from './Pages/SignUpPage';
-import EditPage from './Pages/EditPage';
+import EditPage from './Pages/EditingPage';
 import NotFound from './Pages/NotFound';
 
 function App() {
@@ -29,11 +28,10 @@ function App() {
         <GlobalStyle />
         <AllWrap>
           <Header />
-          <Footer />
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/edit" element={<EditPage />} />
+            <Route path="/edit/:boardId" element={<EditPage />} />
             <Route path="/writing" element={<WritingPage />} />
             <Route path="/detail" element={<DetailPage />} />
             <Route path="/signup" element={<SignUpPage />} />
@@ -43,14 +41,7 @@ function App() {
             <Route path="/main" element={<MainPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-          <ToastContainerStyled
-            limit={1}
-            position="bottom-center"
-            closeButton={false}
-            hideProgressBar
-            transition={Slide}
-            autoClose={4000}
-          />
+          <Footer />
         </AllWrap>
         <ToastContainerStyled
           limit={1}
@@ -61,7 +52,6 @@ function App() {
           autoClose={4000}
         />
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
@@ -78,6 +68,7 @@ const ToastContainerStyled = styled(ToastContainer)`
   text-align: center;
   font-weight: 700;
   font-size: 25px;
+
   .Toastify__toast {
     width: 392px;
     height: 100px;
